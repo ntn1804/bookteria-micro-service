e43c6eed0c9c2a074dcffa98ffaa49c4cebadb4a
NghiaNT <ntn1804@gmail.com>
2024-12-12 16:10:39 +0700

    add user profile service

--- profile-service/src/main/java/com/devteria/profile/controller/UserProfileController.java
+++ profile-service/src/main/java/com/devteria/profile/controller/UserProfileController.java
@@ -3,6 +3,8 @@ package com.devteria.profile.controller;
 import com.devteria.profile.dto.request.UserProfileCreationRequest;
 import com.devteria.profile.dto.response.UserProfileResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +22,11 @@ public class UserProfileController {
   @PostMapping
   UserProfileResponse createProfile(@RequestBody UserProfileCreationRequest request) {
     return userProfileService.createUserProfile(request);
+  }
+
+  @GetMapping("/{userId}")
+  UserProfileResponse getUserProfile(@PathVariable String userId) {
+    return userProfileService.getUserProfile(userId);
   }
 
 }
--- profile-service/src/main/java/com/devteria/profile/service/UserProfileService.java
+++ profile-service/src/main/java/com/devteria/profile/service/UserProfileService.java
@@ -3,20 +3,28 @@ package com.devteria.profile.service;
 import com.devteria.profile.dto.request.UserProfileCreationRequest;
 import com.devteria.profile.dto.response.UserProfileResponse;
 import com.devteria.profile.entity.UserProfile;
-import lombok.RequiredArgsConstructor;
 import com.devteria.profile.mapper.UserProfileMapper;
-import org.springframework.stereotype.Service;
 import com.devteria.profile.repository.UserProfileRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserProfileService {
+
   private final UserProfileRepository userProfileRepository;
   private final UserProfileMapper userProfileMapper;
 
   public UserProfileResponse createUserProfile(UserProfileCreationRequest request) {
     UserProfile userProfile = userProfileMapper.toUserProfile(request);
     userProfileRepository.save(userProfile);
+
+    return userProfileMapper.toUserProfileResponse(userProfile);
+  }
+
+  public UserProfileResponse getUserProfile(String userId) {
+    UserProfile userProfile = userProfileRepository.findById(userId)
+        .orElseThrow(() -> new RuntimeException("User Profile not found"));
 
     return userProfileMapper.toUserProfileResponse(userProfile);
   }
