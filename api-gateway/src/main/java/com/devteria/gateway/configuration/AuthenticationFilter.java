12a112e6a9bb40faf355da370ecbcf03df163ea7
NghiaNT <ntn1804@gmail.com>
2025-01-08 16:16:13 +0700

    add request filter in api-gateway

--- api-gateway/src/main/java/com/devteria/gateway/configuration/AuthenticationFilter.java
+++ api-gateway/src/main/java/com/devteria/gateway/configuration/AuthenticationFilter.java
@@ -7,12 +7,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
@@ -25,12 +27,27 @@ import reactor.core.publisher.Mono;
 public class AuthenticationFilter implements GlobalFilter, Ordered {
 
   private final IdentityService identityService;
+  private final ObjectMapper objectMapper;
+
+  private final List<String> publicEndpoints = List.of(
+      "/identity/auth/.*",
+      "/identity/users/registration"
+  );
+
+  @Value("${app.api-prefix}")
+  private String apiPrefix;
 
   @Override
   public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-    log.info("hihi");
+    log.info("api-gateway authenticate...");
+
+    if (isPublicEndpoint(exchange.getRequest())) {
+      return chain.filter(exchange);
+    }
+
     // Get token from request header
     List<String> authHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION);
+
     if (CollectionUtils.isEmpty(authHeader)) {
       return unauthenticated(exchange.getResponse());
     }
@@ -53,10 +70,14 @@ public class AuthenticationFilter implements GlobalFilter, Ordered {
     return -1;
   }
 
+  private boolean isPublicEndpoint(ServerHttpRequest request) {
+    return publicEndpoints.stream()
+        .anyMatch(s -> request.getURI().getPath().matches(apiPrefix + s));
+  }
+
   Mono<Void> unauthenticated(ServerHttpResponse response) {
     ApiResponse<?> apiResponse = ApiResponse.builder().code(401).message("Unauthenticated").build();
 
-    ObjectMapper objectMapper = new ObjectMapper();
     String body = null;
     try {
       body = objectMapper.writeValueAsString(apiResponse);
